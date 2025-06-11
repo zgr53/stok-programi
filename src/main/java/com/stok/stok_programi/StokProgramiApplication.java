1c6a8d498cb3bb6c9591ed88004341b2138978cb
zgr53 <ahmet.gurkan@std.medipol.edu.tr>
2025-06-11 13:15:13 +0300

    Add files via upload

---
+++ src/main/java/com/stok/stok_programi/StokProgramiApplication.java
@@ -0,0 +1,13 @@
+package com.stok.stok_programi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StokProgramiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StokProgramiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stok/stok_programi/StokProgramiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stok.stok_programi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StokProgramiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
